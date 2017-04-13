a9f472b531664ad8be926c4c3b599832c4697835
Keary Chang <yearofthesheep@gmail.com>
2017-04-12 22:46:38 -0400

    update home.html to home.php
    Added PHP tag to call script that generates HTML report from DB query

--- views/home.php
+++ views/home.php
@@ -5,6 +5,7 @@
 </select>
 <a href="report.html" class="btn btn-default">Report Incident</a>
 <table>
+<?php include 'scripts/homeReport.php'; ?>
 <tr><div id="incident">
 	<p><img src="http://vignette2.wikia.nocookie.net/fantendo/images/1/1b/Super-mario-mushroom.jpg/revision/latest?cb=20120603230623"></img>
 	Lorem ipsum dolor sit amet, usu no malorum laoreet. Pro at volumus erroribus similique. Per in nemore sapientem consulatu, in duo possit epicurei urbanitas. Has vero accommodare ne, nam at essent delectus lucilius. Te assum erant animal cum, duis eius verear mei ad.</p>
