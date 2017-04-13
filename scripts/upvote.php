fedaec87c863a8622f078b1733b4dd344ba762ac
Keary Chang <yearofthesheep@gmail.com>
2017-04-13 10:55:07 -0400

    Changed upvote.php to use $_GET instead of $_POST

--- scripts/upvote.php
+++ scripts/upvote.php
@@ -17,9 +17,9 @@ ALTER TABLE votes ADD CONSTRAINT vote_refs2 FOREIGN KEY (accUsername) REFERENCES
 	    die("Connection failed: " . mysqli_connect_error());
 	}
 
-	$reportID = mysqli_real_escape_string($conn, $_POST['id']);
+	$reportID = mysqli_real_escape_string($conn, $_GET['id']);
 	$accUsername = $_SESSION["username"];
-	$vote = mysqli_real_escape_string($conn, $_POST['vote']);
+	$vote = mysqli_real_escape_string($conn, $_GET['vote']);
 
 	$sql = "SELECT COUNT(reportID) FROM report WHERE reportID = $reportID AND accUsername = $accUsername";
 
