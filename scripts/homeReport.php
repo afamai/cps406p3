d2790f1c1869fb0b8312401cacceb9041a4e920a
Keary Chang <yearofthesheep@gmail.com>
2017-04-13 15:48:58 -0400

    Update homeReport.php

--- scripts/homeReport.php
+++ scripts/homeReport.php
@@ -4,17 +4,8 @@
 	    die('Connection failed');
 	}
 
-	$date_sql = "SELECT reportID, reportDate, reportDescript, reportLoc, reportType, reportVotes FROM report WHERE reportStatus = 0 ORDER BY reportDate DESC";
-	$priority_sql = "SELECT reportID, reportDate, reportDescript, reportLoc, reportType, reportVotes FROM report WHERE reportStatus = 0 ORDER BY reportVotes DESC";
-	$img_arr = array(
-			 "Utility Failure" => "assets/utility.png",
-			 "Potholes" => "assets/pothole.png",
-			 "Vandalism" => "assets/graffiti.png",
-			 "Eroded Streets" => "assets/road.png",
-			 "Flooded Streets" => "assets/flood.png",
-			 "Tree Collapse" => "assets/tree.png",
-			 "Mould and Spore Growth" => "assets/mould.png",
-			 "Garbage or Other Road Blocking Objects" => "assets/trash.png");
+	$date_sql = "SELECT reportID, reportDate, reportDescript, reportLoc, reportStatus, reportType, reportVotes FROM report WHERE reportStatus = 0 ORDER BY reportDate DESC";
+	$priority_sql = "SELECT reportID, reportDate, reportDescript, reportLoc, reportStatus, reportType, reportVotes FROM report WHERE reportStatus = 0 ORDER BY reportVotes DESC";
 
 	$date_val = mysqli_query( $conn, $date_sql ); 
 	$priority_val = mysqli_query( $conn, $priority_sql ); 
@@ -22,51 +13,23 @@
 	  die('Could not get data: ' . mysql_error());
 	}
 
+	$count = 0;
 	while($row = mysqli_fetch_assoc($priority_val)) {
-		echo 
-		'<tr> <div id="incident" class="priority"> <p>
-		<div class="row">
-			<img class="col-lg-4" src='.$img_arr[$row['reportType']].'> </img>
-			<h3> <b>'.$row['reportLoc'].'</b> </h3>'
-			.$row['reportDescript'].'<br>
-		</div>
-		<div class="row">
-			Votes - '.$row['reportVotes'].'&emsp;&emsp;&emsp;Date - '.$row['reportDate'].'&emsp;
-			<form action="/scripts/upvote.php" method="post" id="upvoteForm" class="col-lg-4">
-				<input type="hidden" name="vote" value=1>
-				<input type="hidden" name="id" value='.$row['reportID'].'>
-				<button type="button" form="upvoteForm" class="btn btn-success">Upvote</button>
-			</form>
-			<form action="/scripts/upvote.php" method="post" id="downvoteForm" class="col-lg-4">
-				<input type="hidden" name="vote" value=-1>
-				<input type="hidden" name="id" value='.$row['reportID'].'>
-				<button type="button" form="downvoteForm" class="btn btn-danger">Downvote</button>
-			</form>
-		</div>
-		</p> </div> </tr> <br>';
+		$reports_priority[] = array("id" => $row["reportID"], "date" => $row["reportDate"], "description" => $row["reportDescript"], 
+		"location" => $row["reportLoc"], "status" => $row["reportStatus"], "type" => $row["reportType"], "votes" => $row["reportVotes"], "class" => "priority");
+		$count++;
 	}
 	while($row = mysqli_fetch_assoc($date_val)) {
-		echo
-		'<tr> <div id="incident" class="date"> <p>
-		<div class="row">
-			<img class="col-lg-4" src='.$img_arr[$row['reportType']].'> </img>
-			<h3> <b>'.$row['reportLoc'].'</b> </h3>'
-			.$row['reportDescript'].'<br>
-		</div>
-		<div class="row">
-			Votes - '.$row['reportVotes'].'&emsp;&emsp;&emsp;Date - '.$row['reportDate'].'&emsp;
-			<form action="/scripts/upvote.php" method="post" id="upvoteForm" class="col-lg-4">
-				<input type="hidden" name="vote" value=1>
-				<input type="hidden" name="id" value='.$row['reportID'].'>
-				<button type="button" form="upvoteForm" class="btn btn-success">Upvote</button>
-			</form>
-			<form action="/scripts/upvote.php" method="post" id="downvoteForm" class="col-lg-4">
-				<input type="hidden" name="vote" value=-1>
-				<input type="hidden" name="id" value='.$row['reportID'].'>
-				<button type="button" form="downvoteForm" class="btn btn-danger">Downvote</button>
-			</form>
-		</div>
-		</p> </div> </tr> <br>';
+		$reports_date[] = array("id" => $row["reportID"], "date" => $row["reportDate"], "description" => $row["reportDescript"], 
+		"location" => $row["reportLoc"], "status" => $row["reportStatus"], "type" => $row["reportType"], "votes" => $row["reportVotes"], "class" => "date");
+	}
+	if ($count > 0) {
+		//$reports_date = array("reports_date" => $reports_date);
+		$reports_priority = array("reports_priority" => $reports_priority);
+		//echo json_encode($reports_date);
+		echo json_encode($reports_priority);
+	} else {
+		echo -1;
 	}
 
 	mysqli_close($conn);
